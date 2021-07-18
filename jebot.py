f05f16491a2df84dbd71742d582387ceeb60ee3b
meaall-com <67676419+meaall-com@users.noreply.github.com>
2021-07-18 03:36:51 +0300

    Create jebot.py

---
+++ jebot.py
@@ -0,0 +1,211 @@
+import os
+import asyncio
+from urllib.parse import urlparse
+from pyrogram.errors import UserNotParticipant, UserBannedInChannel
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from youtube_dl import YoutubeDL
+from opencc import OpenCC
+from config import Config
+import wget
+from PIL import Image
+
+Jebot = Client(
+   "AnyDL Bot",
+   api_id=Config.APP_ID,
+   api_hash=Config.API_HASH,
+   bot_token=Config.TG_BOT_TOKEN,
+)
+
+YTDL_REGEX = r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
+
+s2tw = OpenCC('s2tw.json').convert
+
+
+
+# https://docs.pyrogram.org/start/examples/bot_keyboards
+# Reply with inline keyboard
+@Jebot.on_message(filters.regex(YTDL_REGEX))
+async def ytdl_with_button(c: Client, message: Message):
+    if message.text:
+        try:
+        # url = callback_query.message.text
+            url = message.text
+            ydl_opts = {
+                'format': 'best[ext=mp4]',
+                'outtmpl': '%(title)s - %(extractor)s-%(id)s.%(ext)s',
+                'writethumbnail': True
+            }
+            with YoutubeDL(ydl_opts) as ydl:
+                await message.reply_chat_action("typing")
+                info_dict = ydl.extract_info(url, download=False)
+            # download
+                ydl.process_info(info_dict)
+            # upload
+                video_file = ydl.prepare_filename(info_dict)
+                task = asyncio.create_task(send_video(message, info_dict,
+                                                  video_file))
+                while not task.done():
+                    await asyncio.sleep(3)
+                    await message.reply_chat_action("upload_document")
+                await message.reply_chat_action("cancel")
+                await message.delete()
+        except Exception as e:
+            await message.reply_text(e)
+        await message.delete()
+
+    if Config.VIDEO_THUMBNAIL == "No":
+        async def send_video(message: Message, info_dict, video_file):
+            basename = video_file.rsplit(".", 1)[-2]
+      # thumbnail
+            thumbnail_url = info_dict['thumbnail']
+            img = wget.download(thumbnail_url)
+            im = Image.open(img).convert("RGB")
+            output_directory = os.path.join(os.getcwd(), "downloads", "meaallh")
+            if not os.path.isdir(output_directory):
+                os.makedirs(output_directory)
+            thumb_image_path = f"{output_directory}.jpg"
+            im.save(thumb_image_path,"jpeg")
+      # info (s2tw)
+    if message.photo:
+        message.download(file_name='ytthumb.png')
+        thumbn = os.path.join(os.getcwd(), "downloads", "ytthumb")
+        thumb_path = f"{thumbn}.png"
+    else:
+        thumb_path = thumb_image_path
+        webpage_url = info_dict['webpage_url']
+        title = s2tw(info_dict['title'])
+        name_channel = info_dict['channel']
+        channel_id = info_dict['channel_id']
+        formated = info_dict['format']
+        caption = f"""{webpage_url} —— {title} —— {name_channel} —— {channel_id} —— {formated}"""
+        duration = int(float(info_dict['duration']))
+        width, height = get_resolution(info_dict)
+    await message.reply_video(
+        video_file, caption=caption, duration=duration,
+        width=width, height=height, parse_mode='HTML',
+        thumb=thumb_path)
+
+        os.remove(video_file)
+        os.remove(thumb_image_path)
+        os.remove(thumbn)
+
+        def get_file_extension_from_url(url):
+            url_path = urlparse(url).path
+            basename = os.path.basename(url_path)
+            return basename.split(".")[-1]
+
+
+        def get_resolution(info_dict):
+            if {"width", "height"} <= info_dict.keys():
+                width = int(info_dict['width'])
+                height = int(info_dict['height'])
+    # https://support.google.com/youtube/answer/6375112
+            elif info_dict['height'] == 1080:
+                width = 1920
+                height = 1080
+            elif info_dict['height'] == 720:
+                width = 1280
+                height = 720
+            elif info_dict['height'] == 480:
+                width = 854
+                height = 480
+            elif info_dict['height'] == 360:
+            width = 640
+            height = 360
+            elif info_dict['height'] == 240:
+                width = 426
+                height = 240
+            return (width, height)
+    elif message.caption:
+        try:
+        # url = callback_query.message.text
+            url = message.caption
+            ydl_opts = {
+                'format': 'best[ext=mp4]',
+                'outtmpl': '%(title)s - %(extractor)s-%(id)s.%(ext)s',
+                'writethumbnail': True
+            }
+            with YoutubeDL(ydl_opts) as ydl:
+                await message.reply_chat_action("typing")
+                info_dict = ydl.extract_info(url, download=False)
+            # download
+                ydl.process_info(info_dict)
+            # upload
+                video_file = ydl.prepare_filename(info_dict)
+                task = asyncio.create_task(send_video(message, info_dict,
+                                                  video_file))
+                while not task.done():
+                    await asyncio.sleep(3)
+                    await message.reply_chat_action("upload_document")
+                await message.reply_chat_action("cancel")
+                await message.delete()
+        except Exception as e:
+            await message.reply_text(e)
+        await message.delete()
+
+    if Config.VIDEO_THUMBNAIL == "No":
+        async def send_video(message: Message, info_dict, video_file):
+      basename = video_file.rsplit(".", 1)[-2]
+      # thumbnail
+      thumbnail_url = info_dict['thumbnail']
+      img = wget.download(thumbnail_url)
+      im = Image.open(img).convert("RGB")
+      output_directory = os.path.join(os.getcwd(), "downloads", "meaallh")
+      if not os.path.isdir(output_directory):
+          os.makedirs(output_directory)
+      thumb_image_path = f"{output_directory}.jpg"
+      im.save(thumb_image_path,"jpeg")
+      # info (s2tw)
+      if message.photo:
+          message.download(file_name='ytthumb.png')
+          thumbn = os.path.join(os.getcwd(), "downloads", "ytthumb")
+          thumb_path = f"{thumbn}.png"
+      else:
+          thumb_path = thumb_image_path
+      webpage_url = info_dict['webpage_url']
+      title = s2tw(info_dict['title'])
+      name_channel = info_dict['channel']
+      channel_id = info_dict['channel_id']
+      formated = info_dict['format']
+      caption = f"""{webpage_url} —— {title} —— {name_channel} —— {channel_id} —— {formated}"""
+      duration = int(float(info_dict['duration']))
+      width, height = get_resolution(info_dict)
+      await message.reply_video(
+          video_file, caption=caption, duration=duration,
+          width=width, height=height, parse_mode='HTML',
+          thumb=thumb_path)
+
+      os.remove(video_file)
+      os.remove(thumb_image_path)
+      os.remove(thumbn)
+
+    def get_file_extension_from_url(url):
+        url_path = urlparse(url).path
+        basename = os.path.basename(url_path)
+        return basename.split(".")[-1]
+
+
+    def get_resolution(info_dict):
+        if {"width", "height"} <= info_dict.keys():
+            width = int(info_dict['width'])
+            height = int(info_dict['height'])
+    # https://support.google.com/youtube/answer/6375112
+        elif info_dict['height'] == 1080:
+            width = 1920
+            height = 1080
+        elif info_dict['height'] == 720:
+            width = 1280
+            height = 720
+        elif info_dict['height'] == 480:
+            width = 854
+            height = 480
+        elif info_dict['height'] == 360:
+            width = 640
+            height = 360
+        elif info_dict['height'] == 240:
+            width = 426
+            height = 240
+        return (width, height)
+
+Jebot.run()
