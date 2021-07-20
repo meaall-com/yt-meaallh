0e7b1c7f7e7f16bb333e7b65a1de2921f56509b5
meaall-com <67676419+meaall-com@users.noreply.github.com>
2021-07-20 21:03:02 +0300

    Update jebot.py

--- jebot.py
+++ jebot.py
@@ -21,66 +21,58 @@ YTDL_REGEX = r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(
 
 s2tw = OpenCC('s2tw.json').convert
 
-
-
-# https://docs.pyrogram.org/start/examples/bot_keyboards
-# Reply with inline keyboard
 @Jebot.on_message(filters.regex(YTDL_REGEX))
 async def ytdl_with_button(c: Client, message: Message):
-    try:
-        # url = callback_query.message.text
-        url = message.text
-        ydl_opts = {
-            'format': 'best[ext=mp4]',
-            'outtmpl': '%(title)s - %(extractor)s-%(id)s.%(ext)s',
-            'writethumbnail': True
-        }
-        with YoutubeDL(ydl_opts) as ydl:
-            await message.reply_chat_action("typing")
-            info_dict = ydl.extract_info(url, download=False)
-            # download
-            ydl.process_info(info_dict)
-            # upload
-            video_file = ydl.prepare_filename(info_dict)
-            task = asyncio.create_task(send_video(message, info_dict,
-                                                  video_file))
-            while not task.done():
-                await asyncio.sleep(3)
-                await message.reply_chat_action("upload_document")
-            await message.reply_chat_action("cancel")
-            await message.delete()
-    except Exception as e:
-        await message.reply_text(e)
-    await message.delete()
+    if messsage.text:
+        try:
+             url = message.text
+             ydl_opts = {
+                'format': 'best[ext=mp4]',
+                'outtmpl': '%(title)s - %(extractor)s-%(id)s.%(ext)s',
+                'writethumbnail': True
+            }
+            with YoutubeDL(ydl_opts) as ydl:
+                await message.reply_chat_action("typing")
+                info_dict = ydl.extract_info(url, download=False)
+                ydl.process_info(info_dict)
+                video_file = ydl.prepare_filename(info_dict)
+                task = asyncio.create_task(send_video(message, info_dict,
+                                                        video_file))
+                while not task.done():
+                    await asyncio.sleep(3)
+                    await message.reply_chat_action("upload_document")
+                await message.reply_chat_action("cancel")
+                await message.delete()
+        except Exception as e:
+            await message.reply_text(e)
+        await message.delete()
+        if Config.VIDEO_THUMBNAIL == "No":
+            async def send_video(message: Message, info_dict, video_file):
+               basename = video_file.rsplit(".", 1)[-2]
+               thumbnail_url = info_dict['thumbnail']
+               img = wget.download(thumbnail_url)
+               im = Image.open(img).convert("RGB")
+               output_directory = os.path.join(os.getcwd(), "downloads", "meaallh")
+               if not os.path.isdir(output_directory):
+                   os.makedirs(output_directory)
+               thumb_image_path = f"{output_directory}.jpg"
+               im.save(thumb_image_path,"jpeg")
+               webpage_url = info_dict['webpage_url']
+               title = s2tw(info_dict['title'])
+               name_channel = info_dict['channel']
+               channel_id = info_dict['channel_id']
+               formated = info_dict['format']
+               caption = f"""{webpage_url} —— {title} —— {name_channel} —— {channel_id} —— {formated}"""
+               duration = int(float(info_dict['duration']))
+               width, height = get_resolution(info_dict)
+               await message.reply_video(
+                   video_file, caption=caption, duration=duration,
+                   width=width, height=height, parse_mode='HTML',
+                   thumb=thumb_image_path)
 
-if Config.VIDEO_THUMBNAIL == "No":
-   async def send_video(message: Message, info_dict, video_file):
-      basename = video_file.rsplit(".", 1)[-2]
-      # thumbnail
-      thumbnail_url = info_dict['thumbnail']
-      img = wget.download(thumbnail_url)
-      im = Image.open(img).convert("RGB")
-      output_directory = os.path.join(os.getcwd(), "downloads", "meaallh")
-      if not os.path.isdir(output_directory):
-          os.makedirs(output_directory)
-      thumb_image_path = f"{output_directory}.jpg"
-      im.save(thumb_image_path,"jpeg")
-      # info (s2tw)
-      webpage_url = info_dict['webpage_url']
-      title = s2tw(info_dict['title'])
-      name_channel = info_dict['channel']
-      channel_id = info_dict['channel_id']
-      formated = info_dict['format']
-      caption = f"""{webpage_url} —— {title} —— {name_channel} —— {channel_id} —— {formated}"""
-      duration = int(float(info_dict['duration']))
-      width, height = get_resolution(info_dict)
-      await message.reply_video(
-          video_file, caption=caption, duration=duration,
-          width=width, height=height, parse_mode='HTML',
-          thumb=thumb_image_path)
 
-      os.remove(video_file)
-      os.remove(thumb_image_path)
+               os.remove(video_file)
+               os.remove(thumb_image_path)
 
 def get_file_extension_from_url(url):
     url_path = urlparse(url).path
